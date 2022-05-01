a74f83f958cf16f9a3293fee5ae66f7b906a6ecc
shdinis <88981906+shdinis@users.noreply.github.com>
2022-05-02 00:21:43 +0500

    Update Methods.java

--- src/Methods.java
+++ src/Methods.java
@@ -61,7 +61,8 @@ public class Methods {
 
     public static void Encrypting(char[] alphabet, int key, String file, String message) {
         var mapAlphabet = aDictionaryFromAnArrayOfChar(alphabet, key);
-        System.out.println("Введите полный путь к *.txt файлу где находится текст:");
+        System.out.println("Введите полный путь к *.txt файлу где находится текст: \n "+
+                          "или введите \"2\" чтобы вернутся в главное меню:");
         boolean loop = true;
         while (loop) {
             Scanner console = new Scanner(System.in);
@@ -70,6 +71,9 @@ public class Methods {
                 inTxt = console.nextLine();
             } else {
                 inTxt = file;
+            }
+            if (inTxt == "2"){
+                break;
             }
             String outTxt = OutTxt(inTxt);
             try (
@@ -82,7 +86,8 @@ public class Methods {
                 }
                 loop = false;
             } catch (FileNotFoundException fileNotFoundException) {
-                System.out.println("Файла по указанному пути нет.\n Ведите полный путь к *.txt файлу:");
+                System.out.println("Файла по указанному пути нет.\n Ведите полный путь к *.txt файлу"+
+                                   "или \"2\" для возврата в главное меню:");
             } catch (IOException e) {
                 e.printStackTrace();
             }
