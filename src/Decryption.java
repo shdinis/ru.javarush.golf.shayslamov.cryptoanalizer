ed9e4a908e154fd0aebb78f05a4ee6686ac40e85
shdinis <88981906+shdinis@users.noreply.github.com>
2022-05-02 01:41:37 +0500

    Update Decryption.java

--- src/Decryption.java
+++ src/Decryption.java
@@ -4,7 +4,7 @@ public class Decryption {
         System.out.println("Здесь код расшифрования текста");
         char[] alphabetCharRevers = Methods.Revers(alphabet.toCharArray());
         System.out.println("Выберите каким ключем расшифровать");
-        int key = Methods.Point(1, alphabetCharRevers.length - 1);
+        int key = Methods.Point(1, Integer.MAX_VALUE);
         Methods.Encrypting(alphabetCharRevers,key,"","Файл успешно расшифрован.\n" +
                 "результат сохранен в ту же папку под названием out.txt"+
                 "\nЖелаешь сделать что-то еще?\n");
